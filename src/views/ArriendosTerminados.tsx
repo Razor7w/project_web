cd96ded4fd5a8d4bd2fb58b3c874fab670d381e3
Seba <seba.carroza@gmail.com>
2025-07-08 02:54:18 -0400

    feat: implement delete functionality for completed rentals in ArriendosTerminados

--- src/views/ArriendosTerminados.tsx
+++ src/views/ArriendosTerminados.tsx
@@ -1,8 +1,17 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useRevalidator } from "react-router-dom";
 import type { Vehiculo } from "../types/arriendo";
+import { eliminarArriendos } from "../services/ArriendoService";
 
 export default function ArriendosTerminados() {
   const vehiculos = useLoaderData() as Vehiculo[];
+  const { revalidate } = useRevalidator();
+
+  const borrar = async (id: string) => {
+    const resp = await eliminarArriendos(id);
+    if (resp === 'Arriendo borrado') revalidate()
+    
+  }
+
   return (
     <>
       <h2 className="text-center">Arriendos Terminados</h2>
@@ -17,6 +26,7 @@ export default function ArriendosTerminados() {
               <th scope="col">Tipo Vehículo</th>
               <th scope="col">Fecha Inicio</th>
               <th scope="col">Fecha Fin</th>
+              <th scope="col">Acciones</th>
             </tr>
           </thead>
           <tbody>
@@ -34,6 +44,11 @@ export default function ArriendosTerminados() {
                       ? arriendo.fechaFin.toLocaleDateString()
                       : arriendo.fechaFin
                     : '----------'}
+                </td>
+                <td>
+                  <button className="btn btn-danger me-1" role="button" onClick={() => borrar(arriendo.id)} >
+                   <i className="bi bi-trash3"></i>
+                  </button>
                 </td>
               </tr>
             ))}
