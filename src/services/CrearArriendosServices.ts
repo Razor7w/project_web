1f0050121c1e63c72d87d24d8f799380e5290263
Seba <seba.carroza@gmail.com>
2025-07-08 01:18:51 -0400

    fix: update CrearArriendo function to validate form data and correct import casing

--- src/services/CrearArriendosServices.ts
+++ src/services/CrearArriendosServices.ts
@@ -1,33 +1,34 @@
 import axios from "axios";
 import { safeParse } from "valibot";
-import { CrearArriendoSchema } from "../types/CrearArriendo";
+import { CrearArriendoSchema } from "../types/crearArriendo";
 
 type CrearArriendoFormData = {
-  fechaInicio: string;
-  fechaFin: string;
   patenteVehiculo: string;
   tipoVehiculo: string;
-  rutUsuario: string;
-  nombreUsuario: string;
+  rutCliente: string;
+  nombreCliente: string;
 };
 
-export async function CrearArriendos(formData: CrearArriendoFormData) {
+export async function CrearArriendos(formData: CrearArriendoFormData): Promise<{ mensaje: string }> {
   try {
     const resultado = safeParse(CrearArriendoSchema, formData);
-    if (!resultado.success) {
-      const url = "http://localhost:3000/api/crearArriendos";
-      await axios.post(url, {
-        fechaInicio: formData.fechaInicio,
-        fechaFin: formData.fechaFin,
+
+    if (resultado.success) {
+      const url = "http://localhost:3000/api/crearArriendo";
+      const response = await axios.post(url, {
         patenteVehiculo: formData.patenteVehiculo,
         tipoVehiculo: formData.tipoVehiculo,
-        rutUsuario: formData.rutUsuario,
-        nombreUsuario: formData.nombreUsuario,
+        rutCliente: formData.rutCliente,
+        nombreCliente: formData.nombreCliente,
       });
-      return { success: true };
+
+      return response.data;
+    } else {
+      return { mensaje: "Datos del formulario no válidos." };
     }
   } catch (error) {
-    console.error("Error no se pudo crear el arriendo:");
-    throw error;
+    console.error("Error no se pudo crear el arriendo:", error);
+    return { mensaje: "Error al crear el arriendo." };
   }
 }
+
