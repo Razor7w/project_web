4a45654003ef6efe64842702f6fbcbc260e88a31
Seba <seba.carroza@gmail.com>
2025-07-08 02:38:29 -0400

    fix: conditionally render Navbar items based on current route

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export default function Navbar() {
+   const location = useLocation();
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid bg-primary">
@@ -17,18 +19,23 @@ export default function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item ">
-              <NavLink to="/Inicio" className="nav-link text-light">
-                Inicio
-              </NavLink>
-            </li>
-          </ul>
-          <li className="nav-item d-flex ">
-            <button className="nav-link  text-light" role="button" aria-expanded="false">
-              Cerrar Sesion
-            </button>
-          </li>
+          {location.pathname !== "/" && (
+            <>
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item ">
+                  <NavLink to="/Inicio" className="nav-link text-light">
+                    Inicio
+                  </NavLink>
+                </li>
+              </ul>
+              <li className="nav-item d-flex ">
+                <button className="nav-link  text-light" role="button" aria-expanded="false">
+                  Cerrar Sesion
+                </button>
+              </li>
+            </>
+          )}
+          
         </div>
       </div>
     </nav>
